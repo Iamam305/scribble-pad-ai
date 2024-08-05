bdbd84933571d029d30c319775e682e0424e5034
iamam305 <ajitesh314mishra@gmail.com>
2024-08-05 17:26:18 +0530

    added https://insigh.to/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ import { Suspense } from "react";
 import Script from "next/script";
 // import { Navbar } from "@/components/shared/navbar";
 import Smartlook from 'smartlook-client'
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   if (typeof window !== 'undefined') {
-    
+
     Smartlook.init('75930a8e78deaf34f6e70b790f3af049c0e166f5')
   }
   return (
@@ -35,6 +38,15 @@ export default function RootLayout({
           {children}
         </main>
         <Toaster />
+        <Suspense>
+          <Button variant={"default"} className="fixed bottom-6 right-6">
+
+            <Link href={"https://insigh.to/b/scribble-pad-ai"} target={"_blank"}>
+              Give Feedback
+            </Link>
+          </Button>
+        </Suspense>
+
         <Script src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "785d79991a9f4fe3b8a100cfe895643f"}' defer />
 
         {/* <Script type='text/javascript'>
